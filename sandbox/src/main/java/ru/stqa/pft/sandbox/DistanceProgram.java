06d92398887cdc93160d6dddb4d647ac00299d28
nikor1337 <79450156+nikor1337@users.noreply.github.com>
2021-07-13 17:24:01 +0400

    hometask for 1-st lesson

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/DistanceProgram.java
@@ -0,0 +1,11 @@
+package ru.stqa.pft.sandbox;
+
+public class DistanceProgram {
+    public static void main(String[] args) {
+        Point a = new Point(12, 2);
+        Point b = new Point(18, 25);
+        System.out.println("Расстояние между точками равно: " + a.distance(a, b));
+
+
+    }
+}
