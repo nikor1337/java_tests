88f9be63b1b7e67667ecdc2cb97391c0b81ebc0d
nikor1337 <79450156+nikor1337@users.noreply.github.com>
2021-07-14 15:31:49 +0400

    Выделен родительский класс для работы с группами и написан тест для удаления групп (2_23)

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTests.java
@@ -1,35 +1,9 @@
 package ru.stqa.pft.addressbook;
 
 import org.testng.annotations.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
 
 
-public class GroupCreationTests {
-    ChromeDriver driver;
-
-    @BeforeMethod
-    public void setUp() throws Exception {
-        driver = new ChromeDriver();
-        driver.get("http://192.168.1.69/addressbook/");
-        driver.manage().window().maximize();
-        login("admin", "secret");
-    }
-
-    private void login(String username, String password) {
-        driver.findElement(By.name("user")).click();
-        driver.findElement(By.name("user")).sendKeys(username);
-        driver.findElement(By.name("pass")).click();
-        driver.findElement(By.name("pass")).sendKeys(password);
-        driver.findElement(By.cssSelector("input:nth-child(7)")).click();
-    }
-
-    @AfterMethod
-    public void tearDown() {
-        driver.quit();
-    }
+public class GroupCreationTests extends TestBase {
 
     @Test
     public void testGroupCreation() {
@@ -40,28 +14,4 @@ public class GroupCreationTests {
         returnToGroupPage();
     }
 
-    private void returnToGroupPage() {
-        driver.findElement(By.linkText("group page")).click();
-    }
-
-    private void submitGroupCreation() {
-        driver.findElement(By.name("submit")).click();
-    }
-
-    private void fillGroupFrom(GroupData groupData) {
-        driver.findElement(By.name("group_name")).click();
-        driver.findElement(By.name("group_name")).sendKeys(groupData.getName());
-        driver.findElement(By.name("group_header")).click();
-        driver.findElement(By.name("group_header")).sendKeys(groupData.getHeader());
-        driver.findElement(By.name("group_footer")).click();
-        driver.findElement(By.name("group_footer")).sendKeys(groupData.getFooter());
-    }
-
-    private void initGroupCreation() {
-        driver.findElement(By.name("new")).click();
-    }
-
-    private void gotoGroupPage() {
-        driver.findElement(By.linkText("groups")).click();
-    }
 }
