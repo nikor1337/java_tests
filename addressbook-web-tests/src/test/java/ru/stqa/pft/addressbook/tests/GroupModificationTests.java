9c46764c0384c0a6a3356a047b87daca2afcb433
nikor1337 <79450156+nikor1337@users.noreply.github.com>
2022-02-04 18:24:21 +0400

    4_44 Изменен список на множество, добавлено сравнение множеств в тесте редактирования групп

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class GroupModificationTests extends TestBase {
@@ -12,15 +13,20 @@ public class GroupModificationTests extends TestBase {
     public void testGroupModification(){
         app.getNavigationHelper().gotoGroupPage();
         if (! app.getGroupHelper().isThereAGroup()) {
-            app.getGroupHelper().createGroup(new GroupData("name1", null, null));
+            app.getGroupHelper().createGroup(new GroupData("test1", null, null));
         }
         List<GroupData> before = app.getGroupHelper().getGrouplist();
         app.getGroupHelper().selectGroup(before.size() - 1);
         app.getGroupHelper().initGroupModification();
-        app.getGroupHelper().fillGroupFrom(new GroupData("name1", "header1", "footer1"));
+        GroupData group = new GroupData(before.get(before.size() - 1).getId(), "test1", "test2", "test3");
+        app.getGroupHelper().fillGroupFrom(group);
         app.getGroupHelper().submitGroupModification();
         app.getGroupHelper().returnToGroupPage();
         List<GroupData> after = app.getGroupHelper().getGrouplist();
         Assert.assertEquals(after.size(), before.size());
+
+        before.remove(before.size() - 1);
+        before.add(group);
+        Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
     }
 }
