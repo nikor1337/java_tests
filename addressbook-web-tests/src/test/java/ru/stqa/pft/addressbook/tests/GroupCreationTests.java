e981d33bde422d5c14957c429b10784e3a14d333
nikor1337 <79450156+nikor1337@users.noreply.github.com>
2022-02-04 18:36:58 +0400

    4_45 Реализовано сравнение множеств в тесте создания групп

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.util.HashSet;
 import java.util.List;
 
 
@@ -13,9 +14,20 @@ public class GroupCreationTests extends TestBase {
     public void testGroupCreation() {
         app.getNavigationHelper().gotoGroupPage();
         List<GroupData> before = app.getGroupHelper().getGrouplist();
-        app.getGroupHelper().createGroup(new GroupData("name1", null, null));
+        GroupData group = new GroupData("name1", null, null);
+        app.getGroupHelper().createGroup(group);
         List<GroupData> after = app.getGroupHelper().getGrouplist();
         Assert.assertEquals(after.size(), before.size() + 1);
+
+        int max = 0;
+        for (GroupData g: after){
+            if (g.getId() > max) {
+                max = g.getId();
+            }
+        }
+        group.setId(max);
+        before.add(group);
+        Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
     }
 
 }
