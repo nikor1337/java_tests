0e61513e08957a4fb3dd8d924efb8e07546dcc76
nikor1337 <79450156+nikor1337@users.noreply.github.com>
2022-02-04 16:09:16 +0400

    4_41 Добавлен выбор группы по индексу

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -13,7 +13,7 @@ public class GroupModificationTests extends TestBase {
         if (! app.getGroupHelper().isThereAGroup()) {
             app.getGroupHelper().createGroup(new GroupData("name1", null, null));
         }
-        app.getGroupHelper().selectGroup();
+        app.getGroupHelper().selectGroup(before - 1);
         app.getGroupHelper().initGroupModification();
         app.getGroupHelper().fillGroupFrom(new GroupData("name1", "header1", "footer1"));
         app.getGroupHelper().submitGroupModification();
