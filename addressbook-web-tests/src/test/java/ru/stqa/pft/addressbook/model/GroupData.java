2067d36e9d21820ab32741347c8547afc881c7b0
nikor1337 <79450156+nikor1337@users.noreply.github.com>
2022-02-06 17:22:16 +0400

    6_64 Реализована загрузка групп из файла XML

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
@@ -1,8 +1,11 @@
 package ru.stqa.pft.addressbook.model;
 
-import java.util.Objects;
+import com.thoughtworks.xstream.annotations.XStreamAlias;
+import com.thoughtworks.xstream.annotations.XStreamOmitField;
 
+@XStreamAlias("group")
 public class GroupData {
+    @XStreamOmitField
     private int id = Integer.MAX_VALUE;
     private String name;
     private String header;
@@ -17,8 +20,8 @@ public class GroupData {
         return this;
     }
 
-    public GroupData withName(String name) {
-        this.name = name;
+    public GroupData withFooter(String footer) {
+        this.footer = footer;
         return this;
     }
 
@@ -27,8 +30,8 @@ public class GroupData {
         return this;
     }
 
-    public GroupData withFooter(String footer) {
-        this.footer = footer;
+    public GroupData withName(String name) {
+        this.name = name;
         return this;
     }
 
@@ -56,12 +59,18 @@ public class GroupData {
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
+
         GroupData groupData = (GroupData) o;
-        return id == groupData.id && Objects.equals(name, groupData.name);
+
+        if (id != groupData.id) return false;
+        return name != null ? name.equals(groupData.name) : groupData.name == null;
+
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, name);
+        int result = id;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        return result;
     }
 }
