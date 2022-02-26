34b8c614bd707dc6187406c9f524827a4ece409d
nikor1337 <79450156+nikor1337@users.noreply.github.com>
2022-02-26 14:42:06 +0400

    8_78 Реализовано взаимодействие с mantis по http протоколу

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
@@ -41,4 +41,12 @@ public class ApplicationManager {
     public void stop() {
         driver.quit();
     }
+
+    public HttpSession newSession() {
+        return new  HttpSession(this);
+    }
+
+    public String getProperty(String key) {
+        return properties.getProperty(key);
+    }
 }
