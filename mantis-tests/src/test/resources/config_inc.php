415113aee33452574921256ee03448d99b63df50
nikor1337 <79450156+nikor1337@users.noreply.github.com>
2022-02-26 18:20:49 +0400

    8_82 Реализована работа с james сервером через telnet

--- mantis-tests/src/test/resources/config_inc.php
+++ mantis-tests/src/test/resources/config_inc.php
@@ -6,4 +6,4 @@ $g_db_username = 'root';
 $g_db_password = '';
 $g_signup_use_captcha = OFF;
 $g_phpMailer_method = PHPMAILER_METHOD_SMTP;
-$g_smtp_host = '192.168.1.68'; // Тут необходимо указывать адрес тачки, с которой запускаются тесты (в случае если виртуалка с мантисом и тачка автотестов разные)
+$g_smtp_host = 'localhost';
